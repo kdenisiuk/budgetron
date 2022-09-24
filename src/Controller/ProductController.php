7a0100377b496553415c542892116f2c6f063ded
kdenisiuk <kdenisiuk1993@gmail.com>
2022-09-24 17:53:59 +0200

    added basic product features

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+use App\Dto\Request\Product\EditProductDto;
+use App\Dto\Request\Product\NewProductDto;
+use App\Form\EditProductForm;
+use App\Form\NewProductForm;
+use App\Service\Product\ProductService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+
+    public function __construct(
+        private readonly ProductService $productService
+    )
+    {
+    }
+
+    #[Route('/product')]
+    public function addProduct(Request $request): RedirectResponse|Response
+    {
+        $productDto = new NewProductDto();
+
+        $form = $this->createForm(NewProductForm::class, $productDto);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $productDto = $form->getData();
+
+            $this->productService->addNewProduct($productDto);
+
+            return $this->redirectToRoute('app_product_addproduct');
+        }
+
+        return $this->renderForm('addProduct.html.twig', ['form' => $form]);
+
+    }
+
+    #[Route('/editProduct/{productId}')]
+    public function editProduct(Request $request): RedirectResponse|Response
+    {
+        $productDto = new EditProductDto();
+
+        $form = $this->createForm(EditProductForm::class, $productDto);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $productDto = $form->getData();
+
+            $this->productService->editProduct($productDto);
+
+            return$this->redirectToRoute('app_product_getproductslist');
+        }
+
+        return $this->renderForm('');
+    }
+
+    #[Route('product/list')]
+    public function getProductsList(): Response
+    {
+        $list = $this->productService->getAllProducts();
+        return $this->render('/Product/allProductsList.html.twig', ['list' => $list]);
+    }
+
+}
---
+++ src/Service/Product/ProductService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Service\Product;
+
+use App\Dto\Request\Product\EditProductDto;
+use App\Dto\Request\Product\NewProductDto;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ProductService
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly ProductRepository $productRepository
+    )
+    {
+    }
+
+    public function addNewProduct(NewProductDto $dto)
+    {
+
+        $product = new Product(
+            $dto->getName(),
+            $dto->getCategory()
+        );
+
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
+    }
+
+    public function editProduct(EditProductDto $dto)
+    {
+
+        $product = $this->productRepository->getProductById($dto->getId());
+
+        if (!is_null($dto->getName())){
+            $product->setName($dto->getName());
+        }
+
+        if (!is_null($dto->getCategory())){
+            $product->setCategory($dto->getCategory());
+        }
+
+        $product->setUpdateDate(new DateTime());
+
+        $this->entityManager->flush();
+    }
+
+    public function getAllProducts(): array
+    {
+        return $this->productRepository->getAllNotRemovedProducts();
+    }
+}
